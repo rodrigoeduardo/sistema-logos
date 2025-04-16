7ff6b75fdd9ede520412d8c790ea67fb08d35822
Rodrigo Eduardo <rodrigoeduardodb1@gmail.com>
2025-04-16 16:26:35 -0300

    feat: implement dice rolling functionality with user input in DiceBox component

--- src/components/dice-box.tsx
+++ src/components/dice-box.tsx
@@ -1,7 +1,10 @@
 "use client";
 
-import { createDice } from "@/lib/dice";
+import { createDice, getDice } from "@/lib/dice";
 import { useEffect, useState, useRef } from "react";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { Dice6 } from "lucide-react";
 
 export default function DiceBox() {
   const [isClient, setIsClient] = useState(false);
@@ -33,6 +36,28 @@ export default function DiceBox() {
     return <div className="w-full h-72 border rounded-xl">Loading dice...</div>;
   }
 
+  const handleRoll = (notation: string) => {
+    const dice = getDice();
+
+    if (!notation) return;
+
+    // Split by + and - to get individual dice expressions
+    const notationArray = notation
+      .toLowerCase()
+      .split(/[+-]/)
+      .map((expr) => expr.trim())
+      .filter((expr) => /^\d+d\d+$/.test(expr));
+
+    console.log(notation, notationArray);
+
+    if (notationArray.length > 0) {
+      dice.roll(notationArray);
+      dice.onRollComplete = (result) => {
+        console.log(result);
+      };
+    }
+  };
+
   return (
     <div className="flex flex-col gap-2">
       <h2 className="text-lg font-bold">Dados</h2>
@@ -40,7 +65,28 @@ export default function DiceBox() {
         ref={diceBoxRef}
         id="dice-box"
         className="w-full h-72 border rounded-xl"
-      ></div>
+      />
+      <div className="flex items-center gap-2">
+        <form
+          className="flex items-center gap-2 w-full"
+          onSubmit={(e) => {
+            e.preventDefault();
+            const formData = new FormData(e.currentTarget);
+            const diceNotation = formData.get("diceNotation") as string;
+            handleRoll(diceNotation);
+          }}
+        >
+          <Input
+            type="text"
+            name="diceNotation"
+            placeholder="Digite os dados... ex.: 1d20, 2d4+1d10, etc."
+            className="w-full"
+          />
+          <Button type="submit" size="icon" className="cursor-pointer">
+            <Dice6 />
+          </Button>
+        </form>
+      </div>
     </div>
   );
 }
